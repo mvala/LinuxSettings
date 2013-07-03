a500529cfcf8a9dea2e4f167e5685d3b2631e275
Martin Vala <Martin.Vala@cern.ch>
2013-07-03 17:52:07 +0400

    Added new version of eide + change size in case of eide

--- emacs/.emacs.d/mvala-ide.el
+++ emacs/.emacs.d/mvala-ide.el
@@ -5,9 +5,12 @@
     (winner-mode 1))
   (if (file-exists-p ".emacs-ide-project.cfg")
       (progn
-	(add-to-list 'load-path "~/.emacs.d/site-lisp/eide/")
+	(add-to-list 'load-path "~/.emacs.d/site-lisp/")
 	(require 'eide)
 	(eide-start)
 	(load-file (concat (file-name-directory default-directory) "etc/emacs-ide-extra.el"))
+	(if window-system
+	    (set-frame-size (selected-frame) 170 45))
+
 	)))
 
--- git/apply.sh
+++ git/apply.sh
@@ -5,3 +5,4 @@ test -d ~/.config || mkdir ~/.config
 cp .gitconfig ~/
 cp .git-prompt.sh ~/
 cp .config/git-prompt.conf ~/.config/
+git config --global push.default simple
