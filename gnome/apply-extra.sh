93082e90c743bf77dc0860f8b6aa00a66c18fd32
Martin Vala <Martin.Vala@cern.ch>
2013-11-26 20:01:15 +0100

    removed gnome settings

--- gnome/apply-extra.sh
+++
@@ -1,2 +0,0 @@
-# scroll
-gsettings set org.gnome.settings-daemon.peripherals.touchpad scroll-method 'edge-scrolling'
--- gnome/clear.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -Rf ~/.gnome2/ ~/.gnome2_private ~/.gconf ~/.gtk-* ~/.imsettings.log
