1d01382919ef8074c6f178f29645d35a999090fc
Martin Vala <mvala@saske.sk>
2014-08-10 23:22:36 +0400

    Remove check for $DIPLAY variable
    Fix prompt display on machines without $DISPLAY defined

--- bash/.config/.prompt.sh
+++ bash/.config/.prompt.sh
@@ -16,8 +16,5 @@ if [ "$PS1" ];then
     export PROMPT_DIRTRIM=1
     export GIT_PS1_SHOWDIRTYSTATE=true
     export GIT_PS1_SHOWUNTRACKEDFILES=true
-    if [ -n "$DISPLAY" ];then
-        export PS1="\[\033[01;37m\]\$? \$(if [[ \$? == 0 ]]; then echo \"\[\033[01;32m\]\342\234\223\"; else echo \"\[\033[01;31m\]\342\234\227\"; fi) $(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]\h'; else echo '\[\033[01;32m\]\u@\h'; fi)\[\033[01;34m\] \W\[\033[01;33m\]\$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] "
-    fi
+    export PS1="\[\033[01;37m\]\$? \$(if [[ \$? == 0 ]]; then echo \"\[\033[01;32m\]\342\234\223\"; else echo \"\[\033[01;31m\]\342\234\227\"; fi) $(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]\h'; else echo '\[\033[01;32m\]\u@\h'; fi)\[\033[01;34m\] \W\[\033[01;33m\]\$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] "
 fi
-
